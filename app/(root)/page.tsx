abc10b7449e10c558eab63b15921673b27e6c0dd
Siddharth Singh <siddharth.singh+gitlab@coriolis.co.in>
2025-05-12 21:39:36 +0530

    Home page header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,42 @@
-const Home = () => {
-  return <div>Home</div>;
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
+const page = () => {
+  return (
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready with AI-powered Practice & Feedback</h2>
+          <p className="text-lg">
+            Practice on real interview questions and get instant feedback
+          </p>
+          <Button className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview </Link>
+          </Button>
+        </div>
+        <Image
+          src="/robot.png"
+          alt="robo-dude"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Your interviews</h2>
+        <div className="interviews-section">
+          <p>You haven&apos;t taken any interviews yet</p>
+        </div>
+      </section>
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take an interview</h2>
+        <div className="interviews-section">
+          <p>There are no interviews available</p>
+        </div>
+      </section>
+    </>
+  );
 };
 
-export default Home;
+export default page;
