4d4559d8fd99fd213f0a56bfb5ee0cfecefd8919
Siddharth Singh <siddharth.singh+gitlab@coriolis.co.in>
2025-05-16 09:28:24 +0530

    Protected the routes

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,17 @@
+import { isAuthenticated } from "@/lib/actions/auth.action";
+import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 
-const AuthLayout = ({ children }: { children: ReactNode }) => {
+const AuthLayout = async ({ children }: { children: ReactNode }) => {
+  //If a user is logged in and tries to access the sign-in page without logging out, redirect to the home page
+  const isUserAuthenticated = await isAuthenticated();
+
+  if (isUserAuthenticated) {
+    return redirect("/");
+  }
+
   return <div className="auth-layout">{children}</div>;
 };
 
 export default AuthLayout;
+ 
--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -77,3 +77,38 @@ export const setSessionCookie = async (idToken: string) => {
     sameSite: "lax",
   });
 };
+
+export const getCurrentUser = async (): Promise<User | null> => {
+  const cookieStore = await cookies();
+  const sessionCookie = cookieStore.get("session")?.value;
+
+  if (!sessionCookie) {
+    return null;
+  }
+
+  try {
+    const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+
+    const userRecord = await db
+      .collection("users")
+      .doc(decodedClaims.uid)
+      .get();
+
+    if (!userRecord.exists) {
+      return null;
+    }
+
+    return {
+      ...userRecord.data(),
+      id: userRecord.id,
+    } as User;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+export const isAuthenticated = async (): Promise<boolean> => {
+  const user = await getCurrentUser();
+  return !!user; //returns true if user exists, converts into boolean
+};
